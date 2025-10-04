d58e9c54fa621b051a445dd873c9010bb60cfc8e
Abdelrazek Kandil <abdraz@alpha-drive-app.com>
2025-10-04 06:57:01 +0300

    Increase retry attempts for DeliverWebhook class from 1 to 3

--- backend/app/Jobs/DeliverWebhook.php
+++ backend/app/Jobs/DeliverWebhook.php
@@ -15,7 +15,7 @@ class DeliverWebhook implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-    public int $tries = 1;
+    public int $tries = 3;
     public int $timeout = 60;
 
     /**
